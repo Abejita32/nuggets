e323f4c13cc1f8913e2fc66eae7e9d291bef48f3
fabian <fabiangalvezcueto2004@gmail.com>
2025-04-10 17:05:38 -0500

    primer commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa tu numero");
+            string txt = Console.ReadLine();
+            int .TryParse (txt,out int val);
+        }
+    }
+}
