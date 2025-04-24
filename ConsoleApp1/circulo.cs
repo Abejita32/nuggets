ab93d08865b7f99ae3be3e9ffaa3a8ed4e370987
Alumno Chacarilla <alumnotlsch@campustls.local>
2025-04-24 16:30:05 -0500

    pollo

---
+++ ConsoleApp1/circulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class circulo
+    {
+        public float r;
+        public circulo(float r)
+        {
+            this.r = r;
+        }
+        public float area() => 3.14f * r * r;
+    }
+}
---
+++ ConsoleApp1/cuadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class cuadrado
+    {
+        public float e;
+        public float f;
+
+        public cuadrado(float e, float f)
+        {
+            this.e=e;
+            this.f=f;
+        } 
+        public float area=>e*f;
+    }   
+}
